ab543bb501869239ad00be1c78c69b5472f30d7b
tcpolymath <40719495+tcpolymath@users.noreply.github.com>
2019-01-09 16:14:11 -0600

    Update sbi_update_member_db.py

--- sbi_update_member_db.py
+++ sbi_update_member_db.py
@@ -569,7 +569,7 @@ if __name__ == "__main__":
                         member = Member(sponsor, shares, timestamp)
                         member.append_share_age(timestamp, shares)
                         member_data[sponsor] = member
-                        member_data[sponsor]["balance_rshares"] = (minimum_upvote_threshold * 5)
+                        member_data[sponsor]["balance_rshares"] = (minimum_vote_threshold * 5)
                     else:
                         
                         member_data[sponsor]["latest_enrollment"] = timestamp
@@ -588,7 +588,7 @@ if __name__ == "__main__":
                             member = Member(s, shares, timestamp)
                             member.append_share_age(timestamp, shares)
                             member_data[s] = member
-                            member_data[s]["balance_rshares"] = (minimum_upvote_threshold * 5)
+                            member_data[s]["balance_rshares"] = (minimum_vote_threshold * 5)
                         else:
                             member_data[s]["latest_enrollment"] = timestamp
                             member_data[s]["shares"] += shares
